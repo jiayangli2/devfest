09fbc7833179dca0a0125389e8b410276c597309
nanahpang <nanahpang@gmail.com>
2018-02-17 18:49:13 -0800

    modify nlp.py

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -13,7 +13,16 @@ document = types.Document(
     type=enums.Document.Type.PLAIN_TEXT)
 
 # Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-print('Text: {}'.format(text1))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+# sentiment = client.analyze_sentiment(document=document).document_sentiment
+response = client.analyze_entities(
+     document=document,
+     encoding_type='UTF32',
+)
+for entity in response.entities:
+    print('=' * 20)
+    print('         name: {0}'.format(entity.name))
+    print('         type: {0}'.format(entity.type))
+    print('     metadata: {0}'.format(entity.metadata))
+    print('     salience: {0}'.format(entity.salience))
+# print('Text: {}'.format(text1))
+# print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
