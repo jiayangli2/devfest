adb47095c2080ee64494ae3b977967b271231a3d
Bowen Mao <b.mao@columbia.edu>
2018-02-17 23:24:36 -0500

    A broken version (with login problem)

--- devfest.py
+++ devfest.py
@@ -5,9 +5,6 @@ import hashlib, uuid
 from helper import populate
 import urllib
 from markupsafe import Markup
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./app.db'#/home/prokingsley/devfest/app.db' 
@@ -24,9 +21,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(120), unique=True, nullable=False)
 
-
     def __repr__(self):
         return '<User %r>' % self.username
+
 
 class Event(db.Model):
     id = db.Column(db.String(80), primary_key=True, nullable=False)
@@ -49,22 +46,24 @@ def urlencode_filter(s):
     s = urllib.quote_plus(s)
     return Markup(s)
 
+
 @app.route('/')
 def index():
 
     # DEBUG: this is debugging code to see what request looks like
-  print (request.args)
+    print (request.args)
 
-  #names = ["grace hopper", "alan turing", "ada lovelace"]
-  names = {"grace hopper":[123,"hi"], "alan turing":[456, "yeah"]}
-  context = dict(data = names)
+    #names = ["grace hopper", "alan turing", "ada lovelace"]
+    names = {"grace hopper":[123,"hi"], "alan turing":[456, "yeah"]}
+    context = dict(data = names)
 
-  return render_template("index.html", **context)
+    return render_template("index.html", **context)
+
 
 @app.route('/login', methods=['POST'])
 def login():
     uname = request.form['username']
-    pwd = hashlib.sha512((request.form['password'] + salt).encode('ascii')).hexdigest()
+    pwd = hashlib.sha512(request.form['password']).hexdigest()
     try:
         temp = User.query.filter_by(username=uname).first()
         if temp == None or temp.password != pwd:
@@ -80,15 +79,17 @@ def login():
         context = dict(data = err_msg)
         return render_template("index.html", **context)
 
+
 @app.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
         print ("hello world!")
         uname = request.form['username']
-        pwd = hashlib.sha512((request.form['password'] + salt).encode('ascii')).hexdigest()
+        pwd = hashlib.sha512(request.form['password']).hexdigest()
         fname = request.form['fullname']
         email = request.form['email']
         phone = request.form['phone']
+        app.logger.info('%s logged in successfully', uname)
         user_created = User(username = uname, fullname = fname, password = pwd, phone = phone, email = email)
         try:
             db.session.add(user_created)
@@ -105,16 +106,15 @@ def signup():
     context = dict(data = err_msg)
     return render_template("index.html", **context)
 
+
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    session.pop('fullname', None)
-    session.pop('phone', None)
-    session.pop('email', None)
+    for key in session.keys():
+        session.pop(key)
     return redirect(url_for('index'))
 
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
